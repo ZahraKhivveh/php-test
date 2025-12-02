6fb0de772dbbce45cf11f9f001564c227ef3e8a2
zahra khivveh <zahrakhivveh@gmail.com>
2025-12-03 00:47:46 +0330

    php1404/09/11

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
-<?php
+<!-- <?php
 echo "hello world";
-$username ="maryam";
-echo $username;
-echo "khivveh1";
-echo "khivveh2";
-echo "khivveh3";
-echo "mimi"
-?>
+$number = 1404 ;
+echo $number ;
+define('myconst' , 'username');
+echo myconst ;
+ 
+echo "zaki";
+echo "hello world"; ?>
+ -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+ </head>
+ <body>
+    <?php
+echo "salam";
+echo 1+1;
+    ?>
+ </body>
+ </html>
